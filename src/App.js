16a10c9b58caf6bf14b6e4adb7219a86ac0ff261
hansol na <pinecone171@gmail.com>
2023-12-11 16:08:34 +0900

    임시 로그인 화면

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+//import KakaoMap from './Components/KakaoMap/KakaoMap';
+import Login from './Components/User/Login';
+import NaverLoginHandler from './Components/User/NaverLoginHandler';
+import KakaoLoginHandler from './Components/User/KakaoLoginHandler';
 
 function App() {
+  // return (
+  //   <div className='App'>
+  //     <KakaoMap
+  //       searchWord={'약국'}
+  //       address={'서울특별시 마포구 백범로 23'}
+  //     />
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/oauth/redirected/kakao'
+          element={<KakaoLoginHandler />}
+        />
+        <Route
+          path='/oauth/redirected/naver'
+          element={<NaverLoginHandler />}
+        />
+      </Routes>
     </div>
   );
 }
